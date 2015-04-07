9cb9d3f5acb51efb56b56c48de5858e9e964b501
LMPerson <theliammoran@gmail.com>
2015-04-07 14:18:18 -0500

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,21 @@
+// handles abstract game mechanics
+class game
+{
+    // flips stack above pancakes
+    void flip_pancakes(int spatula_position) { return; }
+    
+    // function to set "minimum" amount of moves
+    void daugherity_function() { return; }
+    
+    // adds a move to the movie counter
+    void add_move() { return; }
+    
+    // sets difficulty of game
+    struct difficulty set_difficulty() { return; }
+    
+    // sets difficulty of game and changes game accordingly
+    void initialize_pancakes(struct difficulty) { return; }
+    
+    // cyclical function to repeat on every move
+    void cycle() { return; }
+};
