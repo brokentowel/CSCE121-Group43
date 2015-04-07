018b858fc135e4737690f70c4742a29a61993edf
LMPerson <theliammoran@gmail.com>
2015-04-07 14:27:36 -0500

    Create leaderboard.cpp

---
+++ leaderboard.cpp
@@ -0,0 +1,21 @@
+// handles the score system and the leaderboard elements
+class leaderboard
+{
+    // data structure for scores with initials, consider using an enumeration of structs to hold position
+    struct score_entry { string initials; int score; };
+    
+    // vector to hold scores in position
+    vector<score_entry> scores;
+    
+    // return high scores 
+    vector<score_entry> get_high_scores() { reutrn scores; }
+    
+    // add a new high score
+    void write_high_score(vector<score_entry> high_scores) { scores.push_back(score_entry); }
+    
+    // returns initals of certain score
+    string get_initials() { return score_entry.initials; }
+    
+    // sets initials of certain score
+    void write_high_score() { return; }
+};
