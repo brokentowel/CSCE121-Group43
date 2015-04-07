c0f67cdd690423e6c36ac23c79cae48528130963
LMPerson <theliammoran@gmail.com>
2015-04-07 14:34:31 -0500

    Create splash_screen.cpp

---
+++ splash_screen.cpp
@@ -0,0 +1,9 @@
+// handles the splash screen/intro/tutorial
+class splash_screen
+{
+    // displays the instructions for the game
+    show_tutorial() { return; }
+    
+    // shows splash_screen as the game starts up
+    splash_screen() { return; }
+};
