591d9ba17b3fd5cf455dc0e195e24372484eaac7
brokentowel <kzp0003@tamu.edu>
2015-04-07 15:01:11 -0500

    Update flip_flaps.cpp

--- flip_flaps.cpp
+++ flip_flaps.cpp
@@ -1,4 +1,12 @@
 int main()
-{
+try {
     return 0;
+}
+catch(exception& e) {
+	cerr << "exception: " << e.what() << '\n';
+	return 1;
+}
+catch (...) {
+	cerr << "Some exception\n";
+	return 2;
 }
