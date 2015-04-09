99e598679ac9b67190eac4306c7c3b2d6316960d
brokentowel <kzp0003@tamu.edu>
2015-04-09 15:32:21 -0500

    Update leaderboard.cpp

--- leaderboard.cpp
+++ leaderboard.cpp
@@ -4,31 +4,49 @@ class leaderboard
     // data structure for scores with initials, consider using an enumeration of structs to hold position
     struct score_entry { string initials; int score; };
     
+    // function for comparing two score_entry
+    bool is_lower(const score_entry& x, const score_entry& x) { return x.score < y.score; }
+    
     // vector to hold scores in position
     vector<score_entry> scores[6];
     
     // return high scores 
     vector<score_entry> get_high_scores() { return scores; }
     
-    ofstream ost {"scores.txt"};
-    string initials;
-    int score;
-    while(ist >> initials >> score)
-    {
-        scores.push_back(score_entry{name,score});
-    }
-    ost.close();
-    
-    ifstream ist {"scores.txt"};
-  
     // add a new high score
-    void write_high_score(vector<score_entry> high_scores) { scores.push_back(score_entry); }
+    void set_leaderboard(score_entry &s)
+    {
+        ofstream ost {"scores.txt"};
+        ifstream ist {"scores.txt"}; // ist reads from the file named iname
+        if (!ist) error("can't open input file ",iname);
+        string initials;
+        int score;
+        while(ist >> initials >> score)
+        {
+            scores.push_back(score_entry{name,score});
+        }
+        ost.close();
+        scores[6]=s;
+        sort(scores.begin(), scores.end(), is_lower);
+        
+       
+        
+        
+        
+    }
+    
+    
+  
+
     
     // returns initals of certain score
     string get_initials() { return score_entry.initials; }
     
     // sets initials of certain score
     void write_high_score() { return; }
+    
+    //write top 5 scores to file and save it
+    ifstream ist {"scores.txt"};
 };
 
 
