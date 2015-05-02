7e2f9575f914247c0f8c46ee5b41d4bf57163fb4
brokentowel <kzp0003@tamu.edu>
2015-05-02 00:07:13 -0500

    Create instructions.cpp

---
+++ instructions.cpp
@@ -0,0 +1,11 @@
+/*
+Welcome to flipjacks! A pancake sorting brain-teaser.
+1. First enter your initials
+2. Select a difficulty (how many pancakes you want to sort)
+3. When the game begins click one of the button at the point you wish to insert a spatula and flip
+4. When you have arranged the pancake so that they are stack by decreasing size, you've finished
+Try to blah blah blah number of tries blah blah gme over
+
+
+
+*/
