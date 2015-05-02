a81a6b30ee26c9cc3c4f688e3f80aa2b6bdd1d48
brokentowel <kzp0003@tamu.edu>
2015-05-01 23:57:53 -0500

    Update leaderboard.cpp

--- leaderboard.cpp
+++ leaderboard.cpp
@@ -7,8 +7,14 @@ class leaderboard
     // data structure for scores with initials, consider using an enumeration of structs to hold position
     struct score_entry { string initials; int score; };
     
+    // function to create a score_entry
+    void create_user(string& init, int p) 
+    {
+    	
+    }
+    
     // function for comparing two score_entry
-    bool is_lower(const score_entry& x, const score_entry& x) { return x.score < y.score; }
+    // bool is_lower(const score_entry& x, const score_entry& x) { return x.score < y.score; }
     
     // vector to hold scores in position
     vector<score_entry> scores[6];
@@ -19,28 +25,27 @@ class leaderboard
     // add a new high score
     void set_leaderboard(score_entry &s)
     {
-        ofstream ost {"scores.txt"};
+    //read current leaderboard from leaderboard.txt
         ifstream ist {"scores.txt"}; // ist reads from the file named iname
         if (!ist) error("can't open input file ",iname);
         string initials;
         int score;
         while(ist >> initials >> score)
+        //fill a vector with leaderboard data of score and initials
         {
-            scores.push_back(score_entry{name,score});
+            scores.push_back(score_entry{initials,score});
         }
-        ost.close();
+        ist.close();
+        ofstream ost {"scores.txt"};
         scores[6]=s;
         sort(scores.begin(), scores.end(), is_lower);
         
-        for (i = 0; i < scores.size(); i++)
+        for (i = 0; i < scores.size()-1; i++)
         {
-                //write this shit to a file
+		ost << scores[i].initials << "	" << scores[i].score << "\n";		
         }        
-		
-        for (i=0;i<scores.size(),i++)
-        {
-                //write this shit to a file
-		}
+	ost.close()	
+       
     }
    
     
@@ -53,8 +58,6 @@ class leaderboard
     // sets initials of certain score
     void write_high_score() { return; }
     
-    //write top 5 scores to file and save it
-    ifstream ist {"scores.txt"};
 };
 
 */
