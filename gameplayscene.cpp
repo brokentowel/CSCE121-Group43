18570e90cf90f93a9ee6af8fd026c0329e7009ac
Frinighty <Fssunesara@gmail.com>
2015-05-02 22:51:58 -0500

    Update gameplayscene.cpp

--- gameplayscene.cpp
+++ gameplayscene.cpp
@@ -98,7 +98,7 @@ bool gameplayscene::wait_for_button() //need to get red x to work
 //------------------------------------------------------------------------------
 void gameplayscene::cb_quit(Address, Address pw)
 {
-	reference_to<gameplayscene>(pw).quit;
+	reference_to<gameplayscene>(pw).quit();
 }
 
 void gameplayscene::cb_spatula1(Address, Address a)
