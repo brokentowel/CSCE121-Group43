15924cb32627ff53211fd1a9d4aab0a3e4355ef3
brokentowel <kzp0003@tamu.edu>
2015-05-02 21:03:28 -0500

    Update Leaderboard_window.cpp

--- Leaderboard_window.cpp
+++ Leaderboard_window.cpp
@@ -1,1 +1,47 @@
+/*
+   Leaderboard_window.cpp
+   Revised form of Simple_window.cpp to conform to project needs
+   Kenneth Payne		5/2/2015
+*/
+
+#include "Leaderboard_window.h"
+
+using namespace Graph_lib;
+
+//------------------------------------------------------------------------------
+
+Leaderboard_window::Leaderboard_window(Point xy, int w, int h, const string& title) :
+    Window(xy,w,h,title),
+    done_button(Point(65,100), 70, 30, "Done", cb_done),
+         [](Address, Address pw){reference_to<Leaderboard_window>(pw).done();},
+{
+	attach(done_button);
+}
+
+//------------------------------------------------------------------------------
+void Leaderboard_window::done()
+{
+    done_pushed = true;
+	hide();
+	//return to SPLASH SCREEN()
+}
+//------------------------------------------------------------------------------
+
+bool Leaderboard_window::wait_for_button() //need to get red x to work
+{
+    show();
+    done_pushed = false;
+#if 1
+    // Simpler handler
+    while (!done_pushed) Fl::wait();
+    Fl::redraw();
+#else
+    // To handle the case where the user presses the X button in the window frame
+    // to kill the application, change the condition to 0 to enable this branch.
+    Fl::run();
+#endif
+    return done_pushed;
+}
+
+
 
