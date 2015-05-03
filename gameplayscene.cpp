75d1c6b54588d33c4940278bc050d0b94bd0b5fa
Frinighty <Fssunesara@gmail.com>
2015-05-03 15:22:36 -0500

    Update gameplayscene.cpp
    added button images

--- gameplayscene.cpp
+++ gameplayscene.cpp
@@ -3,6 +3,7 @@ gameplayscene.cpp
 */
 
 #include "gameplayscene.h"
+#include "Graph.h"
 
 using namespace Graph_lib;
 
@@ -10,24 +11,36 @@ using namespace Graph_lib;
 
 gameplayscene::gameplayscene(Point xy, int w, int h, const string& title) :
 Window(xy, w, h, title),
+background_with_color(Point(0,0),800,600),
 quit_button(Point(729, 0), 70, 30, "Quit", cb_quit),
+quit_b(Point(729,0),70,30),
 spatula1_button(Point(0, 560), 50, 30, "1", cb_spatula1),
+spatula1_b(Point(0,560),50,30),
 spatula1_pushed(false),
 spatula2_button(Point(0, 520), 50, 30, "2", cb_spatula2),
+spatula2_b(Point(0,520),50,30),
 spatula2_pushed(false),
 spatula3_button(Point(0, 480), 50, 30, "3", cb_spatula3),
+spatula3_b(Point(0,480),50,30),
 spatula3_pushed(false),
 spatula4_button(Point(0, 440), 50, 30, "4", cb_spatula4),
+spatula4_b(Point(0,440),50,30),
 spatula4_pushed(false),
 spatula5_button(Point(0, 400), 50, 30, "5", cb_spatula5),
 spatula5_pushed(false),
+spatula5_b(Point(0,400),50,30),
 spatula6_button(Point(0, 360), 50, 30, "6", cb_spatula6),
+spatula6_b(Point(0,360),50,30),
 spatula6_pushed(false),
 spatula7_button(Point(0, 320), 50, 30, "7", cb_spatula7),
+spatula7_b(Point(0,320),50,30),
 spatula7_pushed(false),
 spatula8_button(Point(0, 280), 50, 30, "8", cb_spatula8),
+spatula8_b(Point(0,280),50,30),
 spatula8_pushed(false)
 {
+	background_with_color.set_fill_color(Color::cyan);
+	attach(background_with_color);
 	attach(quit_button);
 	attach(spatula1_button);
 	attach(spatula2_button);
@@ -37,6 +50,25 @@ spatula8_pushed(false)
 	attach(spatula6_button);
 	attach(spatula7_button);
 	attach(spatula8_button);
+	quit_b.set_fill_color(Color::red);
+	attach(quit_b);
+	spatula1_b.set_fill_color(Color::white);
+	attach(spatula1_b);
+	spatula2_b.set_fill_color(Color::white);
+	attach(spatula2_b);
+	spatula3_b.set_fill_color(Color::white);
+	attach(spatula3_b);
+	spatula4_b.set_fill_color(Color::white);
+	attach(spatula4_b);
+	spatula5_b.set_fill_color(Color::white);
+	attach(spatula5_b);
+	spatula6_b.set_fill_color(Color::white);
+	attach(spatula6_b);
+	spatula7_b.set_fill_color(Color::white);
+	attach(spatula7_b);
+	spatula8_b.set_fill_color(Color::white);
+	attach(spatula8_b);
+	
 }
 //------------------------------------------------------------------------------
 void gameplayscene::quit()
