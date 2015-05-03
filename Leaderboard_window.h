89512d65103ff8410017dfaf5199e24f9a2b8f44
brokentowel <kzp0003@tamu.edu>
2015-05-02 20:52:28 -0500

    Update Leaderboard_window.h

--- Leaderboard_window.h
+++ Leaderboard_window.h
@@ -1,1 +1,30 @@
-//create a window type for ldr() to display
+#ifndef LEADERBOARDH_GUARD
+#define LEADERBOARDH_GUARD
+
+#include "GUI.h"    // for Simple_window only (doesn't really belong in Window.h)
+#include "Graph.h"
+
+using namespace Graph_lib;
+
+//------------------------------------------------------------------------------
+
+struct Leaderboard_window : Graph_lib::Window {
+    Leaderboard(Point xy, int w, int h, const string& title );
+	
+    bool wait_for_button(); // simple event loop
+
+protected:
+
+    Button done_button;
+    bool done_pushed;
+  
+    static void cb_done(Address, Address);
+   
+    void done();
+
+};
+
+//------------------------------------------------------------------------------
+
+#endif // LEADERBOARDH_GUARD
+
