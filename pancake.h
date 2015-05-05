1f1ab04446165912a53a2c5b29a70d5bc26a65c9
Frinighty <Fssunesara@gmail.com>
2015-05-04 23:02:16 -0500

    Create pancake.h

---
+++ pancake.h
@@ -0,0 +1,13 @@
+#ifndef pancakeH_GUARD
+#define pancakeH_GUARD
+
+#include "Graph.h"
+#include "std_lib_facilities_4.h"
+using namespace Graph_lib;
+// create pancake class subclass of rectangle activating all member functions/values to be modified and used for rectangle/shape
+class pancake :public Rectangle{
+public:
+    pancake(Point xy,int w,int h);
+private:
+};
+#endif 
