a0bf3990400e8fb51a04a7d70239edca2e87e112
Frinighty <Fssunesara@gmail.com>
2015-05-04 23:01:49 -0500

    Update pancake.cpp

--- pancake.cpp
+++ pancake.cpp
@@ -1,22 +1,9 @@
-#include "Graph.h"
-#include "std_lib_facilities_4.h"
+//create a class for pancake
 
-//create class pancake
-//ch14.3.2 maybe use 	struct Circle : Shape{/*...*/};
-//	or		class Circle : public Shape{public:/*...*/};
-//	that way we can inherit all the functions of shape or override them
-/*
-class Pancake {
-	int width, height; 
-	public: 
-	//public constructor function
-	void Pancake(int);
-	//public swap pancake function
-	void swap_pancake();
-};
-//pancake constructor 
-Pancake::Pancake(int w) {
-	Graph_lib::Rectangle unburnt(Point(300,40),w,20);
-	Graph_lib::Rectangle burnt(Point(300,20),w,20);
-};
-*/
+#include "pancake.h"
+
+pancake::pancake(Point xy,int w,int h)
+:Rectangle(xy,w,h)
+{
+    
+}
