324c97d3b6c35b386472e7c740901299be5cb996
brokentowel <kzp0003@tamu.edu>
2015-05-05 22:37:01 -0500

    Update Leaderboard_window.cpp

--- Leaderboard_window.cpp
+++ Leaderboard_window.cpp
@@ -11,10 +11,38 @@
 #include "Window.h"
 #include "std_lib_facilities_4.h"
 
+bool is_lower(const score_entry& x, const score_entry& y) { return x.score > y.score; }
+
+void set_leaderboard(score_entry& you)
+{
+		vector<score_entry> scores;
+		score_entry s = you;
+		
+	//read current leaderboard from leaderboard.txt
+		ifstream ist {"scores.txt"}; 
+		if (!ist) error("can't open input file ");
+		string initials;
+		int score;
+		while(ist >> initials >> score)
+		{
+			scores.push_back(score_entry{initials,score});
+		}
+		ist.close();
+	//insert player's score into the vector 
+		scores.push_back(s);
+	//sort the vector
+		sort(scores.begin(), scores.end(), is_lower);
+	//write top 5 scores back to scores.txt	
+		ofstream ost {"scores.txt"};
+		for (int i = 0; i < scores.size()-1; i++)
+		{
+			ost << scores[i].initials << '\t' << scores[i].score << "\r\n";		
+		}        
+		ost.close();
+}
+
 using namespace Graph_lib;
-
 //------------------------------------------------------------------------------
-
 Leaderboard_window::Leaderboard_window(Point xy, int w, int h, const string& title) :
     Window(xy,w,h,title),
     back_button(Point(0, 0), 50, 20, "Back", cb_back),
