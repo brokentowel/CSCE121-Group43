823c9a8c136d6bdd79ac4c329d2a4f02e0c6b619
brokentowel <kzp0003@tamu.edu>
2015-05-08 16:49:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@
 #include "std_lib_facilities_4.h"
 #include "Splash_screen.h"
 #include "Graph.h"
-//#include "Leaderboard_window.h"
 
 int main()
 try {
-		if(H112 != 201401L)error("Error: incorrect std_lib_facilities_4.h version ",
-		H112);		
-		
-		Splash_screen win1(Point(100,200),800,600,"Flipjack");
-
-		win1.wait_for_button();
-		return 0;
+	if(H112 != 201401L)error("Error: incorrect std_lib_facilities_4.h version ",
+	H112);		
+	
+	//"starts" the game by popping up a splash screen
+	Splash_screen win1(Point(100,200),800,600,"Flipjack");
+	
+	win1.wait_for_button();
+	return 0;
 }
 catch(exception& e) {
 	cerr << "exception: " << e.what() << '\n';
@@ -25,37 +25,4 @@ catch (...) {
 	return 2;
 }
 
-/*#include "classes.h"
-#include "std_lib_facilities_4.h"
 
-#include "GUI.h"
-#include "Graph.h"
-	
-int main()
-try {
-	Intro introduction(800, 600, "FlipFlaps");
-	return gui_main();								// keeps the window open
-	
-
-	Intro win {Point{100, 100}, 800, 600, "FlipFlaps"};
-	
-	cout << "Select difficulty [2-9]: ";
-	int diff;
-	cin >> diff;
-	
-	if (diff >= 2 && diff <= 9)
-		Game game(diff);
-	else
-		cout << "wrong difficulty entered" << endl;
-	
-}
-
-catch(exception& e) {
-	cerr << "exception: " << e.what() << '\n';
-	return 1;
-}
-
-catch (...) {
-	cerr << "Some exception\n";
-	return 2;
-}*/
