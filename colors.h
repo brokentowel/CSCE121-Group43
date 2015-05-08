8709e4f68423c9fa3400b5bc02656fca235a7596
brokentowel <kzp0003@tamu.edu>
2015-05-08 16:31:20 -0500

    Create colors.h

---
+++ colors.h
@@ -0,0 +1,12 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#include "std_lib_facilities_4.h"
+#include "Graph.h"
+
+	Color pancake_color(fl_rgb_color(214, 151, 43));
+	Color toasty(fl_rgb_color(115, 79, 17));
+	Color blue(fl_rgb_color(33, 152, 237));
+	Color dark_blue(fl_rgb_color(6, 83, 138));
+
+#endif
