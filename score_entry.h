c9a1eca94128c62f2b12688687991f6c702cd36b
brokentowel <kzp0003@tamu.edu>
2015-05-08 16:47:48 -0500

    Update score_entry.h

--- score_entry.h
+++ score_entry.h
@@ -2,15 +2,13 @@
 #define SCORE_ENTRY_H
 #include "std_lib_facilities_4.h"
 
+//type to describe a user and his/her score
 class score_entry
 {
   public:
 	string initials; 
 	int score;
 	void set_init(string& init){initials = init;}
-	string get_init(){return initials;}
-	void set_score(int& sc){score = sc;}
-	int get_score(){return score;}
 };
 
 #endif
